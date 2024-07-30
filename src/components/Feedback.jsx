cbb9299a126459f20ee9dbdc30fee7e8c13d97ab
VurradoVi <02kvg03@mail.ru>
2024-07-30 14:31:13 +0300

    work with styledComponents and useRef

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,26 +1,58 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Button from "./Button/Button";
+
+function StateVSRef() {
+  const input = useRef();
+  const [inputValue, setInputValue] = useState('');
+  function handleKey(event) {
+    if (event.key === "Enter") {
+      setInputValue(input.current.value);
+    } 
+  }
+
+  return (
+    <div>
+      <h3>Input value: {inputValue}</h3>
+      <input
+        ref={input}
+        type="text"
+        onKeyDown={handleKey}
+        className="control"
+      />
+    </div>
+  );
+}
 
 export default function Feedback() {
   function handleName(event) {
-    setName(event.target.value);
-    setHasError(event.target.value.trim().length === 0);
+    // setName(event.target.value);
+    // setHasError(event.target.value.trim().length === 0);
+    setForm((prev) => ({
+      ...prev,
+      name: event.target.value,
+      hasError: event.target.value.trim().length === 0,
+    }));
   }
-  const [name, setName] = useState("");
-  const [hasError, setHasError] = useState(true);
-  const [reason, setReason] = useState("help");
+  const [form, setForm] = useState({
+    name: "",
+    hasError: true,
+    reason: "help",
+  });
+  // const [name, setName] = useState("");
+  // const [hasError, setHasError] = useState(false);
+  // const [reason, setReason] = useState("help");
   return (
     <section>
       <h3>Обратная связь</h3>
-      <form>
+      <form style={{ marginBottom: "10px" }}>
         <label htmlFor="name">Ваше имя</label>
         <input
           type="text"
           id="name"
           className="control"
-          value={name}
+          value={form.name}
           style={{
-            border: hasError ? "1px solid red" : null,
+            border: form.hasError ? "1px solid red" : null,
           }}
           onChange={handleName}
         />
@@ -30,16 +62,22 @@ export default function Feedback() {
           name=""
           id="reason"
           className="control"
-          value={reason}
-          onChange={(event) => setReason(event.target.value)}
+          value={form.reason}
+          onChange={(event) =>
+            setForm((prev) => ({ ...prev, reason: event.target.value }))
+          }
         >
           <option value="error">Ошибка</option>
           <option value="help">Нужна помощь</option>
           <option value="da">Да</option>
           <option value="net">Нет</option>
         </select>
-        <Button disabled={hasError} isActive={!hasError}>Отправить</Button>
+        <Button disabled={form.hasError} isActive={!form.hasError}>
+          Отправить
+        </Button>
       </form>
+
+      <StateVSRef />
     </section>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
 import logo from "/logo-name.svg";
 import { useState } from "react";
+import { styled } from "styled-components";
+
+const HeaderContainer = styled.header`
+  height: 50px;
+  display: flex;
+  padding: 0 2rem;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+  background: #fafafa;
+`;
 
 export default function Header() {
   const [now, setNow] = useState(new Date());
@@ -8,10 +19,10 @@ export default function Header() {
   }, 1000);
 
   return (
-    <header>
+    <HeaderContainer>
       {/* <h3>Courses</h3> */}
       <img src={logo} alt="" />
       <span>Время сейчас: {now.toLocaleTimeString()}</span>
-    </header>
+    </HeaderContainer>
   );
 }
