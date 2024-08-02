51c35b301de76585bc4c979d94c0525da76d54dd
VurradoVi <02kvg03@mail.ru>
2024-08-02 16:46:39 +0300

    async, await, twoHooks

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,27 +1,34 @@
 import { useEffect, useState } from "react";
 import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
+import useInput from "../hooks/useinput";
 
 export default function EffectSection() {
+  const input = useInput();
   const [modal, setModal] = useState(false);
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
 
-  async function fetchUsers() {
-    setLoading(true);
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
-    setUsers(users);
-    setLoading(false);
-  }
-
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function fetchUsers() {
+      setLoading(true);
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const users = await response.json();
+      setUsers(users);
+      setLoading(false);
+    }
+    fetchUsers();
+  }, []);
 
   return (
     <section>
       <h3>Effect</h3>
 
-      <Button onClick={() => setModal(true)}>Открыть инфо</Button>
+      <Button style={{ marginBottom: "1rem" }} onClick={() => setModal(true)}>
+        Открыть инфо
+      </Button>
 
       <Modal open={modal}>
         <h3>Hello from modal</h3>
@@ -38,6 +45,23 @@ export default function EffectSection() {
           Закрыть инфо
         </Button>
       </Modal>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && (
+        <>
+          <input type="text" className="control" {...input} />
+          <ul>
+            {users
+              .filter((user) =>
+                user.name.toLowerCase().includes(input.value.toLowerCase())
+              )
+              .map((user) => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+          </ul>
+        </>
+      )}
     </section>
   );
 }
